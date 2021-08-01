d86170e40580a7f196a2467b8c7eeefd9417a8c1
Patrick Mineault <patrick.mineault@gmail.com>
2021-08-01 15:46:39 -0400

    ZMQ example

---
+++ labgraph/examples/zmq_source.py
@@ -0,0 +1,30 @@
+import numpy as np
+import time
+import zmq
+
+SAMPLE_RATE = 10.0
+NUM_FEATURES = 100
+ENDPOINT = "tcp://*:5555"
+TOPIC = b"randomstream"
+
+# Send samples at the sample rate using zmq. 
+def stream_samples(socket):
+    c = "|/-\\"
+    n = 0
+    while True:
+        arr = np.random.rand(NUM_FEATURES)
+        socket.send_multipart([
+            TOPIC,
+            arr
+        ])
+        time.sleep(1 / SAMPLE_RATE)
+        n += 1
+        print(f"\r{c[n % 4]}", end="")
+
+
+if __name__ == '__main__':
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind(ENDPOINT)
+
+    stream_samples(socket)
