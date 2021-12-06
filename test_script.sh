f1d83c4da2425fc54b5b7344cbeb4834e75a4264
jf <jinyu_feng@mfe.berkeley.edu>
2021-12-06 14:53:21 -0800

    Update setup and test_script

--- test_script.sh
+++ test_script.sh
@@ -2,16 +2,16 @@
 # RUN export LC_ALL=C.UTF-8
 # RUN export LANG=en_US.utf-8
 
-python3.6 -m pytest --pyargs -v labgraph._cthulhu
-python3.6 -m pytest --pyargs -v labgraph.events
-python3.6 -m pytest --pyargs -v labgraph.graphs
-python3.6 -m pytest --pyargs -v labgraph.loggers
-python3.6 -m pytest --pyargs -v labgraph.messages
-python3.6 -m pytest --pyargs -v labgraph.runners.tests.test_process_manager
-python3.6 -m pytest --pyargs -v labgraph.runners.tests.test_aligner
-python3.6 -m pytest --pyargs -v labgraph.runners.tests.test_cpp
-python3.6 -m pytest --pyargs -v labgraph.runners.tests.test_exception
-python3.6 -m pytest --pyargs -v labgraph.runners.tests.test_launch
-python3.6 -m pytest --pyargs -v labgraph.runners.tests.test_runner
-python3.6 -m pytest --pyargs -v labgraph.zmq_node
-python3.6 -m pytest --pyargs -v labgraph.devices.protocols.lsl
+python3 -m pytest --pyargs -v labgraph._cthulhu
+python3 -m pytest --pyargs -v labgraph.events
+python3 -m pytest --pyargs -v labgraph.graphs
+python3 -m pytest --pyargs -v labgraph.loggers
+python3 -m pytest --pyargs -v labgraph.messages
+python3 -m pytest --pyargs -v labgraph.runners.tests.test_process_manager
+python3 -m pytest --pyargs -v labgraph.runners.tests.test_aligner
+python3 -m pytest --pyargs -v labgraph.runners.tests.test_cpp
+python3 -m pytest --pyargs -v labgraph.runners.tests.test_exception
+python3 -m pytest --pyargs -v labgraph.runners.tests.test_launch
+python3 -m pytest --pyargs -v labgraph.runners.tests.test_runner
+python3 -m pytest --pyargs -v labgraph.zmq_node
+python3 -m pytest --pyargs -v labgraph.devices.protocols.lsl
