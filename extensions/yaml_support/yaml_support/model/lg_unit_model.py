067539c2becadaf48db2462b6c3d7b030e78770d
bennaaym <mr.aymen.bennabi@gmail.com>
2021-12-11 14:42:40 +0300

    bennaaym/yaml_support: redundant parentheses removed

--- extensions/yaml_support/yaml_support/model/lg_unit_model.py
+++ extensions/yaml_support/yaml_support/model/lg_unit_model.py
@@ -2,12 +2,8 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 
 from .base_model import BaseModel
-from extensions.yaml_support.yaml_support.serializer.yaml_serializer import (
-    YamlSerializer
-)
-from extensions.yaml_support.yaml_support.enums.lg_units_enum import (
-    LabGraphBuiltinUnits
-)
+from extensions.yaml_support.yaml_support.serializer.yaml_serializer import YamlSerializer
+from extensions.yaml_support.yaml_support.enums.lg_units_enum import LabGraphBuiltinUnits
 from typing import Dict, Any
 
 
