4ea00b6e1400fdd45ad1918996f3740fba565db9
Yunusbcr <biceryu@gmail.com>
2022-01-05 02:17:26 -0500

    key update

--- devices/hardware/delsys/delsys/delsys.py
+++ devices/hardware/delsys/delsys/delsys.py
@@ -19,26 +19,9 @@ class Delsys():
 
     def __init__(self):
 
-        self.key = "MIIBKjCB4wYHKoZIzj0CATCB1wIBATAsBgcqhkjOPQEBAiEA/////wAAAAEAAAAAAAAAAAAAAAD///////////////8wWwQg/////wAAAAEAAAAAAAAAAAAAAAD///////////////wEIFrGNdiqOpPns+u9VXaYhrxlHQawzFOw9jvOPD4n0mBLAxUAxJ02CIbnBJNqZnjhE50mt4GffpAEIQNrF9Hy4SxCR/i85uVjpEDydwN9gS3rM6D0oTlF2JjClgIhAP////8AAAAA//////////+85vqtpxeehPO5ysL8YyVRAgEBA0IABP1Xm+7r9iKdX6tiPOAeqYY9WB18iIcN4JZ3F7jKcdwSVMfoHvPixv6UaOuDaxhL5fKXPmOOdqzY3xYrTROA6OI="
+        self.key = ""
 
-        self.license = "<License>"\
-            "<Id>a23e567a-b0f0-4d90-a0d5-794d845926d2</Id>"\
-            "<Type>Standard</Type>"\
-            "<Quantity>10</Quantity>"\
-            "<LicenseAttributes>"\
-            "<Attribute name=\"Software\">VS2012</Attribute>"\
-            "</LicenseAttributes>"\
-            "<ProductFeatures>"\
-            "<Feature name=\"Sales\">True</Feature>"\
-            "<Feature name=\"Billing\">False</Feature>"\
-            "</ProductFeatures>"\
-            "<Customer>"\
-            "<Name>Yu Bicer</Name>"\
-            "<Email>biceryu@gmail.com</Email>"\
-            "</Customer>"\
-            "<Expiration>Wed, 01 Jan 2031 05:00:00 GMT</Expiration>"\
-            "<Signature>MEUCICTyzglHLhoh3rCOAvzVWE6jjKQKgQjOkRply/NaexBfAiEArrJz3zg8C98YCTGHtk8SmCiQ+u6P7Oy8OlKBULqVmHY=</Signature>"\
-            "</License>"
+        self.license = ""
 
         self.TrigBase = AeroPy()
 
