1983088722844876006b21bfbf47d7088dfd7b9c
bennaaym <mr.aymen.bennabi@gmail.com>
2022-02-18 22:44:26 +0300

    bennaaym/labgraph: update from graph/ to Graph/ issue resolved

---
+++ extensions/prototypes/labgraph_monitor/src/components/Graph/interfaces/IEdge.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+interface IEdge {
+    id: string;
+    label?: string;
+    source: string;
+    target: string;
+    arrowHeadType?: 'arrow' | 'arrowclosed';
+    type?: 'default' | 'straight' | 'step' | 'smoothstep';
+    animated?: boolean;
+    style?: { [property: string]: string };
+}
+
+export default IEdge;
--- extensions/prototypes/labgraph_monitor/src/components/Graph/types/TGraphElement.ts
+++ extensions/prototypes/labgraph_monitor/src/components/Graph/types/TGraphElement.ts
@@ -4,5 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-export { default as SettingPanel } from './SettingPanel/SettingPanel';
-export { default as Graph } from './Graph/Graph';
+import IEdge from '../interfaces/IEdge';
+import INode from '../interfaces/INode';
+export type TGraphElement = INode | IEdge;
