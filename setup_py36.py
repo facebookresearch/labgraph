e0911bc79c9a5ce712139f62236bb71180323841
jf <jinyu_feng@mfe.berkeley.edu>
2022-03-23 12:12:13 -0700

    Update for typing_extensions

--- setup_py36.py
+++ setup_py36.py
@@ -43,7 +43,7 @@ setup(
         "pytest_mock==2.0.0",
         "pyzmq==18.1.0",
         "typeguard==2.5.1",
-        "typing_extensions>=3.7.4.3",
+        "typing_extensions==3.7.4.3",
         "websockets==8.1",
         "yappi==1.2.5",
     ],
