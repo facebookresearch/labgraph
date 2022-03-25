6babf640018eb3a04ec50a3902e502d3ceae159a
Damir Temir <dtemi2@uis.edu>
2022-03-25 10:58:25 -0500

    WIP real-time messaging example

--- extensions/yaml_support/labgraph_monitor/server/lg_monitor_server.py
+++ extensions/yaml_support/labgraph_monitor/server/lg_monitor_server.py
@@ -11,7 +11,20 @@ from .serializer_node import SerializerConfig, Serializer
 from ..aliases.aliases import SerializedGraph
 from .enums.enums import ENUMS
 
-APP_ID = 'LABGRAPH_MONITOR'
+# Add additional nodes
+from ....graphviz_support.graphviz_support.tests.demo_graph.noise_generator import NoiseGenerator, NoiseGeneratorConfig
+from ....graphviz_support.graphviz_support.tests.demo_graph.amplifier import Amplifier, AmplifierConfig
+from ....graphviz_support.graphviz_support.tests.demo_graph.attenuator import Attenuator, AttenuatorConfig
+from ....graphviz_support.graphviz_support.tests.demo_graph.rolling_averager import RollingAverager, RollingConfig
+
+# Needed test configurations
+NUM_FEATURES = 100
+WINDOW = 2.0
+REFRESH_RATE = 2.0
+OUT_IN_RATIO = 1.2
+ATTENUATION = 5.0
+
+APP_ID = 'LABGRAPH.MONITOR'
 WS_SERVER = ENUMS.WS_SERVER
 STREAM = ENUMS.STREAM
 DEFAULT_IP = WS_SERVER.DEFAULT_IP
@@ -19,40 +32,86 @@ DEFAULT_PORT = WS_SERVER.DEFAULT_PORT
 DEFAULT_API_VERSION = WS_SERVER.DEFAULT_API_VERSION
 SAMPLE_RATE = 5
 
+class WSSenderNode(lg.Graph):
+        SERIALIZER: Serializer
+        WS_SERVER_NODE: WSAPIServerNode
+
+        NOISE_GENERATOR: NoiseGenerator
+        ROLLING_AVERAGER: RollingAverager
+        AMPLIFIER: Amplifier
+        ATTENUATOR: Attenuator
+
+        def setup_data(self, data) -> None:
+            self.data = data
+
+        def setup(self) -> None:
+            self.WS_SERVER_NODE.configure(
+                WSAPIServerConfig(
+                    app_id=APP_ID,
+                    ip=WS_SERVER.DEFAULT_IP,
+                    port=ENUMS.WS_SERVER.DEFAULT_PORT,
+                    api_version=ENUMS.WS_SERVER.DEFAULT_API_VERSION,
+                    num_messages=-1,
+                    enums=ENUMS(),
+                    sample_rate=SAMPLE_RATE,
+                )
+            )
+            self.SERIALIZER.configure(
+                SerializerConfig(
+                    data=self.data,
+                    sample_rate=SAMPLE_RATE,
+                    stream_name=STREAM.LABGRAPH_MONITOR,
+                    stream_id=STREAM.LABGRAPH_MONITOR_ID
+                )
+            )
+            self.NOISE_GENERATOR.configure(
+            NoiseGeneratorConfig(
+                sample_rate=float(SAMPLE_RATE),
+                num_features=NUM_FEATURES
+                )
+            )
+
+            self.ROLLING_AVERAGER.configure(
+                RollingConfig(window=WINDOW)
+            )
+
+            self.AMPLIFIER.configure(
+                AmplifierConfig(out_in_ratio=OUT_IN_RATIO)
+            )
+
+            self.ATTENUATOR.configure(
+                AttenuatorConfig(attenuation=ATTENUATION)
+            )
+
+        def connections(self) -> lg.Connections:
+            return (
+                (self.NOISE_GENERATOR.OUTPUT, self.ROLLING_AVERAGER.INPUT),
+                (self.NOISE_GENERATOR.OUTPUT, self.AMPLIFIER.INPUT),
+                (self.NOISE_GENERATOR.OUTPUT, self.ATTENUATOR.INPUT),
+                (self.NOISE_GENERATOR.OUTPUT, self.SERIALIZER.INPUT_1),
+                (self.ROLLING_AVERAGER.OUTPUT, self.SERIALIZER.INPUT_2),
+                (self.AMPLIFIER.OUTPUT, self.SERIALIZER.INPUT_3),
+                (self.ATTENUATOR.OUTPUT, self.SERIALIZER.INPUT_4),
+                (self.SERIALIZER.TOPIC, self.WS_SERVER_NODE.topic),
+            )
+
+        def process_modules(self) -> Tuple[lg.Module, ...]:
+            return (
+                self.NOISE_GENERATOR,
+                self.ROLLING_AVERAGER,
+                self.AMPLIFIER,
+                self.ATTENUATOR,
+                self.SERIALIZER, 
+                self.WS_SERVER_NODE,
+            )
 
 def run_server(data: SerializedGraph) -> None:
     """
     A function that creates a Websocket server graph.
     The server graph streams the lagraph topology to the clients
     """
-    class WSSenderNode(lg.Graph):
-        SERIALIZER: Serializer
-        WS_SERVER_NODE: WSAPIServerNode
+    WS = WSSenderNode()
+    WS.setup_data(data)
 
-        def setup(self) -> None:
-            wsapi_server_config = WSAPIServerConfig(
-                app_id=APP_ID,
-                ip=WS_SERVER.DEFAULT_IP,
-                port=ENUMS.WS_SERVER.DEFAULT_PORT,
-                api_version=ENUMS.WS_SERVER.DEFAULT_API_VERSION,
-                num_messages=-1,
-                enums=ENUMS(),
-                sample_rate=SAMPLE_RATE,
-            )
-            self.SERIALIZER.configure(
-                SerializerConfig(
-                    data=data,
-                    sample_rate=SAMPLE_RATE,
-                    stream_name=STREAM.LABGRAPH_MONITOR,
-                    stream_id=STREAM.LABGRAPH_MONITOR_ID
-                )
-            )
-            self.WS_SERVER_NODE.configure(wsapi_server_config)
-
-        def connections(self) -> lg.Connections:
-            return ((self.SERIALIZER.TOPIC, self.WS_SERVER_NODE.topic),)
-
-        def process_modules(self) -> Tuple[lg.Module, ...]:
-            return (self.SERIALIZER, self.WS_SERVER_NODE, )
-
-    lg.run(WSSenderNode)
+    runner = lg.ParallelRunner(graph=WS)
+    runner.run()
--- extensions/yaml_support/labgraph_monitor/server/serializer_node.py
+++ extensions/yaml_support/labgraph_monitor/server/serializer_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2004-present Facebook. All Rights Reserve
 
+from typing import Optional
 import labgraph as lg
 import asyncio
 from labgraph.websockets.ws_server.ws_server_stream_message import (
@@ -8,6 +9,9 @@ from labgraph.websockets.ws_server.ws_server_stream_message import (
 )
 from ..aliases.aliases import SerializedGraph
 
+# Make it work with RandomMessage
+from ....graphviz_support.graphviz_support.tests.demo_graph.random_message import RandomMessage
+import numpy as np
 
 class SerializerConfig(lg.Config):
     data: SerializedGraph
@@ -15,6 +19,11 @@ class SerializerConfig(lg.Config):
     stream_name: str
     stream_id: str
 
+class DataState(lg.State):
+    data_1: Optional[np.ndarray] = None
+    data_2: Optional[np.ndarray] = None
+    data_3: Optional[np.ndarray] = None
+    data_4: Optional[np.ndarray] = None
 
 class Serializer(lg.Node):
     """
@@ -23,15 +32,53 @@ class Serializer(lg.Node):
 
     TOPIC = lg.Topic(WSStreamMessage)
     config: SerializerConfig
+    state: DataState
 
+    INPUT_1 = lg.Topic(RandomMessage)
+    INPUT_2 = lg.Topic(RandomMessage)
+    INPUT_3 = lg.Topic(RandomMessage)
+    INPUT_4 = lg.Topic(RandomMessage)
+
+    @lg.subscriber(INPUT_1)
+    def add_message_1(self, message: RandomMessage) -> None:
+        self.state.data_1 = message.data
+
+    @lg.subscriber(INPUT_2)
+    def add_message_2(self, message: RandomMessage) -> None:
+        self.state.data_2 = message.data
+
+    @lg.subscriber(INPUT_3)
+    def add_message_3(self, message: RandomMessage) -> None:
+        self.state.data_3 = message.data
+        
+    @lg.subscriber(INPUT_4)
+    def add_message_4(self, message: RandomMessage) -> None:
+        self.state.data_4 = message.data
+    
     @lg.publisher(TOPIC)
     async def source(self) -> lg.AsyncPublisher:
-        await asyncio.sleep(.01)
+        await asyncio.sleep(.1)
         while True:
-            msg = WSStreamMessage(
-                samples=self.config.data,
+            output_data = {
+                key: value for key, value in self.config.data.items()
+            }
+            # Populate Serializer Node
+            output_data["nodes"]["Serializer"]["upstreams"]["NoiseGenerator"][0]["fields"]["data_content"] = str(self.state.data_1[0])
+            output_data["nodes"]["Serializer"]["upstreams"]["RollingAverager"][0]["fields"]["data_content"] = str(self.state.data_2[0])
+            output_data["nodes"]["Serializer"]["upstreams"]["Amplifier"][0]["fields"]["data_content"] = str(self.state.data_3[0])
+            output_data["nodes"]["Serializer"]["upstreams"]["Attenuator"][0]["fields"]["data_content"] = str(self.state.data_4[0])
+
+            # Populate RollingAverage Node
+            output_data["nodes"]["RollingAverager"]["upstreams"]["NoiseGenerator"][0]["fields"]["data_content"] = str(self.state.data_1[0])
+            
+            # Populate Amplifier Node
+            output_data["nodes"]["Amplifier"]["upstreams"]["NoiseGenerator"][0]["fields"]["data_content"] = str(self.state.data_2[0])
+
+            # Populate Attenuator Node
+            output_data["nodes"]["Attenuator"]["upstreams"]["NoiseGenerator"][0]["fields"]["data_content"] = str(self.state.data_3[0])
+            yield self.TOPIC, WSStreamMessage(
+                samples=output_data,
                 stream_name=self.config.stream_name,
                 stream_id=self.config.stream_id,
             )
-            yield self.TOPIC, msg
-            await asyncio.sleep(1 / self.config.sample_rate)
+            await asyncio.sleep(1 / self.config.sample_rate), 
