b4a1c8c97786cbc27b2fa5c3b79e856d20b5029c
Damir Temir <dtemi2@uis.edu>
2022-03-25 14:01:30 -0500

    WIP updated serialized message representation

--- extensions/yaml_support/labgraph_monitor/server/serializer_node.py
+++ extensions/yaml_support/labgraph_monitor/server/serializer_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2004-present Facebook. All Rights Reserve
 
-from typing import Optional
+from typing import Dict, Optional
 import labgraph as lg
 import asyncio
 from labgraph.websockets.ws_server.ws_server_stream_message import (
@@ -20,10 +20,10 @@ class SerializerConfig(lg.Config):
     stream_id: str
 
 class DataState(lg.State):
-    data_1: Optional[np.ndarray] = None
-    data_2: Optional[np.ndarray] = None
-    data_3: Optional[np.ndarray] = None
-    data_4: Optional[np.ndarray] = None
+    data_1: Optional[Dict] = None
+    data_2: Optional[Dict] = None
+    data_3: Optional[Dict] = None
+    data_4: Optional[Dict] = None
 
 class Serializer(lg.Node):
     """
@@ -41,19 +41,31 @@ class Serializer(lg.Node):
 
     @lg.subscriber(INPUT_1)
     def add_message_1(self, message: RandomMessage) -> None:
-        self.state.data_1 = message.data
+        self.state.data_1 = {
+            "timestamp": message.timestamp,
+            "numpy": list(message.data),
+        }
 
     @lg.subscriber(INPUT_2)
     def add_message_2(self, message: RandomMessage) -> None:
-        self.state.data_2 = message.data
+        self.state.data_2 = {
+            "timestamp": message.timestamp,
+            "numpy": list(message.data),
+        }
 
     @lg.subscriber(INPUT_3)
     def add_message_3(self, message: RandomMessage) -> None:
-        self.state.data_3 = message.data
+        self.state.data_3 = {
+            "timestamp": message.timestamp,
+            "numpy": list(message.data),
+        }
         
     @lg.subscriber(INPUT_4)
     def add_message_4(self, message: RandomMessage) -> None:
-        self.state.data_4 = message.data
+        self.state.data_4 = {
+            "timestamp": message.timestamp,
+            "numpy": list(message.data),
+        }
     
     @lg.publisher(TOPIC)
     async def source(self) -> lg.AsyncPublisher:
@@ -63,19 +75,26 @@ class Serializer(lg.Node):
                 key: value for key, value in self.config.data.items()
             }
             # Populate Serializer Node
-            output_data["nodes"]["Serializer"]["upstreams"]["NoiseGenerator"][0]["fields"]["data_content"] = str(self.state.data_1[0])
-            output_data["nodes"]["Serializer"]["upstreams"]["RollingAverager"][0]["fields"]["data_content"] = str(self.state.data_2[0])
-            output_data["nodes"]["Serializer"]["upstreams"]["Amplifier"][0]["fields"]["data_content"] = str(self.state.data_3[0])
-            output_data["nodes"]["Serializer"]["upstreams"]["Attenuator"][0]["fields"]["data_content"] = str(self.state.data_4[0])
+            output_data["nodes"]["Serializer"]["upstreams"]["NoiseGenerator"][0]["fields"]["timestamp"]["content"] = self.state.data_1["timestamp"]
+            output_data["nodes"]["Serializer"]["upstreams"]["NoiseGenerator"][0]["fields"]["data"]["content"] = self.state.data_1["numpy"]
+            output_data["nodes"]["Serializer"]["upstreams"]["RollingAverager"][0]["fields"]["timestamp"]["content"] = self.state.data_2["timestamp"]
+            output_data["nodes"]["Serializer"]["upstreams"]["RollingAverager"][0]["fields"]["data"]["content"] = self.state.data_2["numpy"]
+            output_data["nodes"]["Serializer"]["upstreams"]["Amplifier"][0]["fields"]["timestamp"]["content"] = self.state.data_3["timestamp"]
+            output_data["nodes"]["Serializer"]["upstreams"]["Amplifier"][0]["fields"]["data"]["content"] = self.state.data_3["numpy"]
+            output_data["nodes"]["Serializer"]["upstreams"]["Attenuator"][0]["fields"]["timestamp"]["content"] = self.state.data_4["timestamp"]
+            output_data["nodes"]["Serializer"]["upstreams"]["Attenuator"][0]["fields"]["data"]["content"] = self.state.data_4["numpy"]
 
             # Populate RollingAverage Node
-            output_data["nodes"]["RollingAverager"]["upstreams"]["NoiseGenerator"][0]["fields"]["data_content"] = str(self.state.data_1[0])
+            output_data["nodes"]["RollingAverager"]["upstreams"]["NoiseGenerator"][0]["fields"]["timestamp"]["content"] = self.state.data_1["timestamp"]
+            output_data["nodes"]["RollingAverager"]["upstreams"]["NoiseGenerator"][0]["fields"]["data"]["content"] = self.state.data_1["numpy"]
             
             # Populate Amplifier Node
-            output_data["nodes"]["Amplifier"]["upstreams"]["NoiseGenerator"][0]["fields"]["data_content"] = str(self.state.data_2[0])
+            output_data["nodes"]["Amplifier"]["upstreams"]["NoiseGenerator"][0]["fields"]["timestamp"]["content"] = self.state.data_2["timestamp"]
+            output_data["nodes"]["Amplifier"]["upstreams"]["NoiseGenerator"][0]["fields"]["data"]["content"] = self.state.data_2["numpy"]
 
             # Populate Attenuator Node
-            output_data["nodes"]["Attenuator"]["upstreams"]["NoiseGenerator"][0]["fields"]["data_content"] = str(self.state.data_3[0])
+            output_data["nodes"]["Attenuator"]["upstreams"]["NoiseGenerator"][0]["fields"]["timestamp"]["content"] = self.state.data_3["timestamp"]
+            output_data["nodes"]["Attenuator"]["upstreams"]["NoiseGenerator"][0]["fields"]["data"]["content"] = self.state.data_3["numpy"]
             yield self.TOPIC, WSStreamMessage(
                 samples=output_data,
                 stream_name=self.config.stream_name,
