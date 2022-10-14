6350685691201801ded0bd93ecd905e2ccfa31d1
Zohaib <zrana1214@gmail.com>
2022-10-14 12:59:08 -0400

    medipipe imports

--- devices/webcam/pose_vis/extensions/face.py
+++ devices/webcam/pose_vis/extensions/face.py
@@ -4,6 +4,14 @@ import labgraph as lg
 import numpy as np
 import mediapipe as mp
 # Import MediaPipe types for intellisense
-import mediapipe.python.solutions.hands as HandsType
+import mediapipe.python.solutions.face_detection as FaceType
 import mediapipe.python.solutions.drawing_utils as DrawingUtilsType
 import mediapipe.python.solutions.drawing_styles as DrawingStylesType
+
+# Every extension will probably need these imports
+from pose_vis.extension import PoseVisExtension, PoseVisConfiguration, ExtensionResult, ResultData
+from pose_vis.stream_combiner import CombinedVideoStream
+from pose_vis.performance_tracking import PerfUtility
+from argparse import ArgumentParser, Namespace
+
+from typing import NamedTuple, Optional, Tuple
