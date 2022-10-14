f254281681d8f183855e2fab415476d9658c2c10
Zohaib <zrana1214@gmail.com>
2022-10-14 12:45:05 -0400

    initial commit for face detection

---
+++ devices/webcam/pose_vis/extensions/face.py
@@ -0,0 +1,9 @@
+import cv2
+import time
+import labgraph as lg
+import numpy as np
+import mediapipe as mp
+# Import MediaPipe types for intellisense
+import mediapipe.python.solutions.hands as HandsType
+import mediapipe.python.solutions.drawing_utils as DrawingUtilsType
+import mediapipe.python.solutions.drawing_styles as DrawingStylesType
