1dc2a879386b4a59ab86ec819997ce926bb63900
Cody Caudill <das@dasfaust.me>
2022-10-20 17:18:25 -0400

    Fix overlay drawing when running multiple extensions

--- devices/webcam/pose_vis/frame_processor.py
+++ devices/webcam/pose_vis/frame_processor.py
@@ -36,7 +36,7 @@ class FrameProcessor():
             overlay, ext_result = ext.process_frame(frame, metadata)
             # TODO: addWeighted() leads to a darker image overall, I'm guessing due to lack of transparency support
             # It is, however extremely fast, faster than other methods I've experimented with
-            overlayed = cv2.addWeighted(overlay, 0.5, frame, 0.5, 0.0)
+            overlayed = cv2.addWeighted(overlay, 0.5, overlayed, 0.5, 0.0)
             ext_results[ext.__class__.__name__] = ext_result.data
         return (overlayed, ext_results)
 
