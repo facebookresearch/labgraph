a2a82af97f4be36beca8ad4096dca3adc9df26e7
Gaurang Ruparelia <gr2159@nyu.edu>
2023-02-09 21:34:44 -0500

    setup speechgpt project and setup the initial layout

---
+++ tree/main/extensions/speechgpt/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+
+      <section className="chatbox">
+
+      </section>
+
+
+    </div>
+  );
+}
+
+export default App;
