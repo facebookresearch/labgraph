8f3f09487c11f22991ed974672eba9c27a8d6f56
Sean Vu <seanvu123@gmail.com>
2023-02-15 07:20:19 -0800

    build on Gaurang's UI and connect backend with frontend and ensure the speechGPT MVP functions like chatGPT

--- tree/main/extensions/backend/index.js
+++ tree/main/extensions/backend/index.js
@@ -1,38 +1,43 @@
-const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
-const express = require('express')
 
-// console.log(process.env.OPENAI_API_KEY)
+
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const configuration = new Configuration({
-    organization: "org-5Ibmla6Cpz4hc0msulYQxwiB",
+    organization: process.env.OPENAI_API_ORG,
     apiKey: process.env.OPENAI_API_KEY,
-});
-// const response = await openai.listEngines();
-
-
+  });
 const openai = new OpenAIApi(configuration);
 
 
-// create a simple express api that calls the function above
+
+
 const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
 
 app.post('/', async (req, res) => {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
-    });
+  const { message } = req.body;
+  console.log(message);
+  const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${message}`,
+      max_tokens: 100,
+      temperature: 0.5,
+  });
 
-    console.log(response.data.choices[0].text);
 
-    res.json({
-        data: response.data
-    })
+  res.json({
+      message: response.data.choices[0].text,
+  })
 })
 
 // listen on port dotenv
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
--- tree/main/extensions/speechgpt/src/App.js
+++ tree/main/extensions/speechgpt/src/App.js
@@ -4,6 +4,54 @@ import { useEffect, useState } from 'react'
 import './normal.css'
 import ChatMessage from './components/ChatMessage';
 
+// function App() {
+//   // add state for input and chat log
+//   const [input, setInput] = useState("");
+//   const [chatLog, setChatLog] = useState([
+//     {
+//       user: "gpt",
+//       message: "How can I help you today?"
+//     }
+//   ]);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     setChatLog([...chatLog, { user: "me", message: `${input}` }, {
+//       user: "me",
+//       message: "I'm sorry, I don't understand"
+//     }]);
+//     setInput("")
+
+//     console.log("hello");
+//   }
+//   return (
+
+//     <div className="App">
+//       <aside className="sidemenu">
+//         <div className="sidemenu-button">
+//           <span>+</span>
+//           New Chat
+//         </div>
+//       </aside>
+
+//       <section className="chatbox">
+//         <div className="chat-log">
+//           {chatLog.map((chat, index) => (
+//             <ChatMessage key={index} user={chat.user} message={chat.message} />
+//           ))}
+//         </div>
+//         <div className="chat-input-holder">
+//           <form onSubmit={handleSubmit}>
+//             <textarea onChange={(e) => setInput(e.target.value)} rows="1" className="chat-input-textarea" placeholder="Type your message here">
+//             </textarea>
+//           </form>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
+
 function App() {
   // add state for input and chat log
   const [input, setInput] = useState("");
@@ -14,16 +62,26 @@ function App() {
     }
   ]);
 
-  const handleSubmit = async (e) => {
+  async function handleSubmit(e) {
     e.preventDefault();
+    console.log('submit')
+    let chatLogNew = [...chatLog, {user: "me", message: `${input}`} ]
+    setInput("");
+    // fetch to the API combining chatlog array of message and sending it as a message to localhost:5000
+    const messages = chatLogNew.map((message)=> message.message).join("\n")
+    const response = await fetch("http://localhost:5000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: messages
+      })
+    });
+    const data = await response.json();
+    setChatLog([...chatLogNew, {user: "gpt", message: `${data.message}`}])
+    console.log(data.message);
 
-    setChatLog([...chatLog, { user: "me", message: `${input}` }, {
-      user: "me",
-      message: "I'm sorry, I don't understand"
-    }]);
-    setInput("")
-
-    console.log("hello");
   }
   return (
 
@@ -42,10 +100,14 @@ function App() {
           ))}
         </div>
         <div className="chat-input-holder">
-          <form onSubmit={handleSubmit}>
-            <textarea onChange={(e) => setInput(e.target.value)} rows="1" className="chat-input-textarea" placeholder="Type your message here">
-            </textarea>
-          </form>
+            <form onSubmit={handleSubmit}>
+            <input
+            rows = "1"
+            value = {input}
+            onChange={(e)=>setInput(e.target.value)}
+            className = "chat-input-textarea">
+            </input>
+            </form>
         </div>
       </section>
     </div>
