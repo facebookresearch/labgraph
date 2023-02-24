ba076983381adab72ebff3b07c4ff5fe25b23699
Gaurang Ruparelia <gr2159@nyu.edu>
2023-02-23 20:39:30 -0500

    enable fb sign in along with google sign in

--- tree/main/extensions/speech-gpt-mobile/components/Login.tsx
+++ tree/main/extensions/speech-gpt-mobile/components/Login.tsx
@@ -15,9 +15,8 @@ const Login = () => {
     height={300}
     alt="logo"
     src="https://firebasestorage.googleapis.com/v0/b/speechgpt-77211.appspot.com/o/Avatar%20ChatGPT.png?alt=media&token=4f789405-6cad-483b-8945-0fbda2722bc1"></Image>
-    {/* <button onClick={() => signIn('facebook')} className="text-3xl font-bold text-white animate-pulse">Sign in to use SpeechGPT</button> */}
-    <button onClick={() => signIn('google')} className="text-3xl font-bold text-white animate-pulse">Sign in to use SpeechGPT</button>
-
+    <button onClick={() => signIn('facebook')} className="text-3xl font-bold text-white animate-pulse">Sign in via Facebook to use SpeechGPT</button>
+    <button onClick={() => signIn('google')} className="text-3xl font-bold text-white animate-pulse">Sign in via Google to use SpeechGPT</button>
     </div>
   )
 }
--- tree/main/extensions/speech-gpt-mobile/pages/api/auth/[...nextauth].ts
+++ tree/main/extensions/speech-gpt-mobile/pages/api/auth/[...nextauth].ts
@@ -1,25 +1,15 @@
-// // write next.js auth code here for facebook login
-
-// import NextAuth from "next-auth"
-// import FacebookProvider from "next-auth/providers/facebook";
-
-// export const authOptions = {
-//     providers: [
-//         FacebookProvider({
-//             clientId: process.env.FACEBOOK_APP_ID!,
-//             clientSecret: process.env.FACEBOOK_APP_SECRET!,
-//         }),
-//     ],
-// }
-
-// export default NextAuth(authOptions)
-
+// write next.js auth code here for facebook login
 
 import NextAuth from "next-auth"
+import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
     providers: [
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_APP_ID!,
+            clientSecret: process.env.FACEBOOK_APP_SECRET!,
+        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_APP_ID!,
             clientSecret: process.env.GOOGLE_APP_SECRET!,
