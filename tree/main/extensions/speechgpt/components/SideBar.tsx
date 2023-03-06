7843c686d1da0d4fabd175e5f4c8c1d14893eab4
Sean Vu <seanvu123@gmail.com>
2023-03-05 23:33:54 -0800

    add styling for Log out button to improve user experience and visual appeal

--- tree/main/extensions/speechgpt/components/SideBar.tsx
+++ tree/main/extensions/speechgpt/components/SideBar.tsx
@@ -53,11 +53,12 @@ function SideBar() {
     {
       session && (
         // log out button
-        <div className="flex items-center justify-center">
-          <button onClick={() => signOut()} className="bg-[#11A37F] text-white p-2 rounded-full hover:bg-[#0F8C6F]">
-            Sign out
-          </button>
-        </div>
+        <a onClick={() => signOut()} className="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
+          <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+            <polyline points="16 17 21 12 16 7"></polyline>
+            <line x1="21" y1="12" x2="9" y2="12"></line>
+          </svg>Log out</a>
         
       )
     }
