7af73529dc6d354a7fd46fd60e951a882821a5c1
Sean Vu <seanvu123@gmail.com>
2023-03-17 14:25:24 -0700

    adjust thumbsUp / thumbsDown feedback

--- tree/main/extensions/speechgpt/components/Message.tsx
+++ tree/main/extensions/speechgpt/components/Message.tsx
@@ -132,10 +132,10 @@ const Message = ({
         {isSpeechGPT && (
           <div className="flex justify-end max-w-2xl px-10 mx-auto">
 
-            <button className="mx-2 hover:text-blue-500" onClick={handleThumbsUp} disabled={thumbsUpClicked}>
+            <button className="mx-2 hover:text-blue-500" onClick={handleThumbsUp} disabled={thumbsUpClicked || message.thumbsDown}>
               <svg stroke="currentColor" fill={message.thumbsUp ? "currentColor" : "none"} stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="w-4 h-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
             </button>
-            <button className="mx-2 hover:text-blue-500" onClick={handleThumbsDown} disabled={thumbsDownClicked}>
+            <button className="mx-2 hover:text-blue-500" onClick={handleThumbsDown} disabled={thumbsDownClicked|| message.thumbsUp}>
               <svg stroke="currentColor" fill={message.thumbsDown ? "currentColor" : "none"} stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="w-4 h-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
             </button>
           </div>
