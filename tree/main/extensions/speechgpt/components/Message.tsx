2592c6f28b2996114754b7266aff281b206528c2
Sean Vu <seanvu123@gmail.com>
2023-03-16 17:48:57 -0700

    build on Guarang's changes to update thumbsUp / thumbsDown value to be stored in database

--- tree/main/extensions/speechgpt/components/Message.tsx
+++ tree/main/extensions/speechgpt/components/Message.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { DocumentData } from "firebase/firestore"
 import { signOut, useSession } from "next-auth/react";
 import { doc, addDoc, getDocs, collection, serverTimestamp, updateDoc } from "firebase/firestore";
-
 import { db } from "../firebase";
 
 type Props = {
@@ -52,6 +51,11 @@ const Message = ({
       setThumbsDownClicked(true);
       setThumbsDownCount(thumbsDownCount + 1);
       setThumbsUpClicked(true);
+      // Get a reference to the specific message you want to update
+      const messageRef = doc(db, 'users', session?.user?.email!, 'chats', chatId, 'messages', messageId);
+
+      // Update the 'thumbsUp' field for the message
+      await updateDoc(messageRef, { thumbsDown: true });
     }
 
   };
@@ -67,10 +71,12 @@ const Message = ({
           <div className="flex justify-end px-10 max-w-2xl mx-auto">
 
             <button className="mx-2" onClick={handleThumbsUp} disabled={thumbsUpClicked}>
-              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg> {thumbsUpCount}
+              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
+              {thumbsUpCount} 
             </button>
             <button className="mx-2" onClick={handleThumbsDown} disabled={thumbsDownClicked}>
-              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>{thumbsDownCount}
+              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
+              {thumbsDownCount} 
             </button>
           </div>
         )}
