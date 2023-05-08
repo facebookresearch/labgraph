177a609f7925e04528d8d8dbbefa998e7f0f46c1
Gaurang Ruparelia <gr2159@nyu.edu>
2023-05-08 01:01:37 -0400

    one more fix

--- tree/main/extensions/speechgpt/components/Login.tsx
+++ tree/main/extensions/speechgpt/components/Login.tsx
@@ -28,16 +28,6 @@ const Login = () => {
         <span className="text-xl">Continue with Facebook</span>
       </button> */}
 
-          <button className="bg-[orange] hover:bg-[#1877F2]/50 text-white font-bold py-4 px-8 rounded-lg flex items-center" onClick={() => signIn('google')}>
-            <div className="bg-[#ffff] w-12 rounded-full flex items-center justify-center mr-4">
-              {/* <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48">
-                <path fill="#1877F2" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"></path>
-                <path fill="#fff" d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"></path>
-              </svg> */}
-            </div>
-            <span className="text-xl">Continue with Google</span>
-          </button>
-
         </div>
 
       </div>
