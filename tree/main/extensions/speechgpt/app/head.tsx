abc8622aa34570753db6d277bf39aaa607e5cb03
Gaurang Ruparelia <gr2159@nyu.edu>
2023-06-08 13:23:22 -0400

    delete extra folders

--- tree/main/extensions/speechgpt/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>SpeechGPT</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/logo_labgraph_icon.ico" />
-    </>
-  )
-}
