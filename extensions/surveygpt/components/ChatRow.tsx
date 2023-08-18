90861a26f71f2a640fe50a8787e55bafc4670925
Kristin Lam <klam623@gmail.com>
2023-08-18 12:18:47 -0400

    Update styles for sidebar

--- extensions/surveygpt/components/ChatRow.tsx
+++ extensions/surveygpt/components/ChatRow.tsx
@@ -1,29 +1,26 @@
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
-import {ChatBubbleLeftIcon, TrashIcon} from "@heroicons/react/24/outline"
+import React, { useEffect, useState } from 'react';
+import { ChatBubbleLeftIcon, TrashIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { collection, deleteDoc, doc, orderBy, query } from 'firebase/firestore';
-import { db } from "../firebase";
+import { db } from '../firebase';
 import { groupCollapsed } from 'console';
 
-
-
 type Props = {
-    id: string;
-    collapsed: boolean;
-  }
-  
+  id: string;
+  collapsed: boolean;
+};
 
-const ChatRow = ({id, collapsed} : Props) => {
+const ChatRow = ({ id, collapsed }: Props) => {
   const pathname = usePathname();
   const router = useRouter();
-  const {data: session} = useSession();
+  const { data: session } = useSession();
   const [active, setActive] = useState(false);
 
-  const[messages] = useCollection(
-      collection(db, 'users', session?.user?.email!, 'chats', id, 'messages')
+  const [messages] = useCollection(
+    collection(db, 'users', session?.user?.email!, 'chats', id, 'messages')
   );
 
   useEffect(() => {
@@ -32,24 +29,34 @@ const ChatRow = ({id, collapsed} : Props) => {
     setActive(pathname.includes(id));
   }, [pathname]);
 
-  const removeChat = async() => {
+  const removeChat = async () => {
     await deleteDoc(doc(db, 'users', session?.user?.email!, 'chats', id));
-    router.replace("/");
-  }
+    router.replace('/');
+  };
 
   return (
     <div>
-    <Link className={`chatRow justify-center ${active && 'bg-gray-700/50'}`} href={`/chat/${id}`}><ChatBubbleLeftIcon className='text-gray-700 w-5 h-5'></ChatBubbleLeftIcon>
-    <p className='flex-1 hidden truncate md:inline-flex text-gray-700/80'>
-      {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
-    </p>
-    {
-      collapsed ? "":<TrashIcon onClick={removeChat} className='w-5 h-5 text-gray-700 hover:text-red-700'></TrashIcon>
-    }
-    </Link> 
+      <Link
+        className={`rounded-lg px-5 py-3 text-sm flex items-center justify-center space-x-2 hover:bg-custom-gray-200 cursor-pointer transition-all duration-200 ease-out ${
+          active && 'bg-custom-gray-100'
+        }`}
+        href={`/chat/${id}`}
+      >
+        <ChatBubbleLeftIcon className="w-5 h-5"></ChatBubbleLeftIcon>
+        <p className="flex-1 hidden truncate md:inline-flex text-gray-700/80">
+          {messages?.docs[messages?.docs.length - 1]?.data().text || 'New Chat'}
+        </p>
+        {collapsed ? (
+          ''
+        ) : (
+          <TrashIcon
+            onClick={removeChat}
+            className="w-5 h-5 text-gray-700 hover:text-red-700"
+          ></TrashIcon>
+        )}
+      </Link>
     </div>
-  )
-  
-}
+  );
+};
 
-export default ChatRow
+export default ChatRow;
--- extensions/surveygpt/components/NewChat.tsx
+++ extensions/surveygpt/components/NewChat.tsx
@@ -1,32 +1,35 @@
-"use client";
+'use client';
 
-import { PlusIcon } from "@heroicons/react/24/solid"
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { db } from "../firebase";
-import { signOut, useSession } from "next-auth/react";
+import { PlusIcon } from '@heroicons/react/24/solid';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
+import { signOut, useSession } from 'next-auth/react';
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 function NewChat() {
   const router = useRouter();
   const { data: session } = useSession();
 
-
-
   const createNewChat = async () => {
     const doc = await addDoc(
-      collection(db, "users", session?.user?.email!, "chats"), {
-      userId: session?.user?.email!,
-      createdAt: serverTimestamp(),
-    })
+      collection(db, 'users', session?.user?.email!, 'chats'),
+      {
+        userId: session?.user?.email!,
+        createdAt: serverTimestamp(),
+      }
+    );
     router.push(`/chat/${doc.id}`);
-  }
+  };
   return (
-    <div onClick={createNewChat} className="border border-gray-700 chatRow">
-      <PlusIcon className="w-4 h-4 text-custom-gray" />
-      <p className="text-custom-gray">New Chat</p>
+    <div
+      onClick={createNewChat}
+      className="rounded-3xl px-5 py-3 flex items-center justify-center space-x-2 bg-custom-blue hover:bg-custom-blue/80 cursor-pointer"
+    >
+      <PlusIcon className="w-4 h-4 text-white" />
+      <p className="text-white">New Chat</p>
     </div>
   );
 }
 
-export default NewChat
+export default NewChat;
