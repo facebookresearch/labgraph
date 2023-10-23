66d95413f5f2c496b6d73229709b867b72eab989
Omar Macias <a01635795@tec.mx>
2023-10-23 15:04:07 -0600

    Add audiogen extension

---
+++ extensions/labgraph_audiogen/README.md
@@ -0,0 +1,3 @@
+# LabGraph AudioGen Extension
+
+A powerful Python command-line tool for Windows. This tool facilitates the creation, processing, and autocompletion high-quality audio and music, using the AudioCraft models, AudioGen, and MusicGen.
---
+++ extensions/labgraph_audiogen/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="labgraph_audiogen",
+    version="0.0.1",
+    description="A command-line interface for the usage of AudioCraft for labgraph",
+    long_description="A command-line interface to facilitate the usage of the models of Audiocraft to generate and process audio on labgraph",
+)
