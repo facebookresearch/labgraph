a6eeb30a5a0e1de6d7545b10c8c050be3b6cd607
Nate8888 <wilk.nathan@gmail.com>
2023-10-26 22:48:52 -0400

    tests the creation of the audio file with desc

--- extensions/labgraph_audiogen/tests/test_main.py
+++ extensions/labgraph_audiogen/tests/test_main.py
@@ -1,5 +1,13 @@
+import os
+import subprocess
+
 def test_main():
-    import subprocess
-    process = subprocess.run(["labgraph_audiogen", "arg1", "arg2"], 
-                             capture_output=True, text=True)
-    assert process.stdout.strip() == "Hello World from labgraph_audiogen with args: arg1 arg2"
+    # Run the script with an example description
+    process = subprocess.run(["labgraph_audiogen", "--description", "dog barking"], capture_output=True, text=True)
+    
+    # Assert that the script ran successfully
+    assert process.returncode == 0, f"Script returned {process.returncode}, expected 0. stdout: {process.stdout}, stderr: {process.stderr}"
+    
+    # Assert that the output file was created
+    assert os.path.exists("0.wav"), "Output file 0.wav was not created"
+    os.remove("0.wav")
