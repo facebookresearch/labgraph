21baef47d8f866c0f72cef22319107e10a500bc9
Omar Macias <a01635795@tec.mx>
2023-10-27 13:22:08 -0600

    feat(audiocraft CLI): cli command now receives arguments to test

--- extensions/labgraph_audiogen/labgraph_audiogen/main.py
+++ extensions/labgraph_audiogen/labgraph_audiogen/main.py
@@ -1,3 +1,8 @@
 import sys
 
-print(sys.argv)
+
+def main():
+    print("Hello World from labgraph_audiogen!")
+    args = sys.argv
+    if args:
+        print(f"Args are: {' '.join(args[1:])}")
--- extensions/labgraph_audiogen/setup.py
+++ extensions/labgraph_audiogen/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
@@ -6,4 +6,12 @@ setup(
     version="0.0.1",
     description="A command-line interface for the usage of AudioCraft for labgraph",
     long_description="A command-line interface to facilitate the usage of the models of Audiocraft to generate and process audio on labgraph",
+    packages=["labgraph_audiogen"],
+    install_requires=[
+        "Click",
+    ],
+    entry_points="""
+        [console_scripts]
+        labgraph_audiogen=labgraph_audiogen.main:main
+    """
 )
