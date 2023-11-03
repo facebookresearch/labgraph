10cc1bebe94b39e08b3f96f40e0807b2c7457e1c
Nate8888 <wilk.nathan@gmail.com>
2023-11-03 13:01:39 -0400

    Checks if file was created

--- extensions/labgraph_audiogen/tests/test_main.py
+++ extensions/labgraph_audiogen/tests/test_main.py
@@ -1,10 +1,10 @@
 import os
 import subprocess
 
-def test_main():
+def test_single_description():
     # Run the script with an example description
-    process = subprocess.run(["labgraph_audiogen", "--description", "dog barking"], capture_output=True, text=True)
+    process = subprocess.run(["labgraph_audiogen", "dog barking", "dog barking"], capture_output=True, text=True)
     
     # Assert that the output file was created
-    assert os.path.exists("0.wav"), "Output file 0.wav was not created"
-    os.remove("0.wav")
+    assert os.path.exists("dog_barking0.wav"), "Output file dog_barking0.wav was not created"
+    os.remove("dog_barking0.wav")
