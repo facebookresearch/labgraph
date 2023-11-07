af8df3d2d58a099abc75ff523f73a177c25bf816
Omar Macias <a01635795@tec.mx>
2023-11-07 10:00:55 -0600

    enhance(audiocraft CLI): musicgen style and validations improved

--- extensions/labgraph_audiogen/labgraph_audiogen/main.py
+++ extensions/labgraph_audiogen/labgraph_audiogen/main.py
@@ -1,29 +1,28 @@
-import sys
 import click
 
 
-def generate_text_music(text, duration, output):
-    click.echo(f"\nGenerating music from text '{text}' with a duration of {duration}s and written on the {output}.wav")
+DEFAULT_AUDIOGEN_MODEL = 'facebook/audiogen-medium'
+DEFAULT_MUSICGEN_MODEL = "facebook/musicgen-medium"
+DEFAULT_AUDIO_DURATION = 10
+
+
+def generate_text_music(text, duration, output, musicgen_model):
     from audiocraft.models import MusicGen
     from audiocraft.data.audio import audio_write
-    model = MusicGen.get_pretrained("facebook/musicgen-medium")
-    model.set_generation_params(
-        use_sampling=True,
-        top_k=250,
-        duration=duration,
-    )
+    click.echo(click.style(f"\nGenerating music from text '{text}' with a duration of {duration}s and written on the {output}.wav", fg="bright_green"))
+    model = MusicGen.get_pretrained(musicgen_model)
+    model.set_generation_params(duration=duration)
     music = model.generate(text, progress=True)
     for generation in music:
-        generation_cpu = generation.cpu()
-        audio_write(f"outputs/{output}", generation_cpu, 32000)
+        audio_write(f"outputs/{output}", generation.cpu(), model.sample_rate)
 
 
 def generate_text_audio(text, duration, output):
-    click.echo(f"\nGenerating audio from text '{text}' with a duration of {duration}s and written on the {output}.wav")
+    click.echo(click.style(f"\nGenerating audio from text '{text}' with a duration of {duration}s and written on the {output}.wav", fg="bright_green"))
     return text
 
 
-@click.command(context_settings={"ignore_unknown_options": True})
+@click.command()
 @click.version_option()
 @click.option("-m", "--model", type=click.Choice(["audiogen", "musicgen"]), help="Select the model to run")
 @click.option("-t", "--text", prompt="Enter the text to generate audio from", prompt_required=False, help="Text to generate audio from")
@@ -31,27 +30,25 @@ def generate_text_audio(text, duration, output):
 @click.option("-o", "--output", prompt="Enter the desired output file name", prompt_required=False, help="Name of the output file")
 def main(model, text, duration, output):
     if not model:
-        click.echo("No model provided, set your model with the -m / --model flag (e.g. labgraph_audiogen -m audiogen)")
-        return sys.exit(1)
+        raise click.BadParameter(click.style("No model provided, set your model with the -m / --model flag (e.g. labgraph_audiogen -m audiogen)", fg="bright_red"))
     if not text:
-        click.echo("No text provided, set your text with the -t / --text flag (e.g. labgraph_audiogen -m audiogen -t 'Hello World')")
-        return sys.exit(1)
+        raise click.BadParameter(click.style("No text provided, set your text with the -t / --text flag (e.g. labgraph_audiogen -m audiogen -t 'Hello World')", fg="bright_red"))
     if not duration:
-        click.echo("No duration provided, set you duration using -d / --duration, default will be used (10 seconds)") 
+        click.echo(click.style("No duration provided, set you duration using -d / --duration, default will be used (10 seconds)", fg="yellow"))
         duration = 10
     else:
         duration = int(duration)
     if not output:
-        click.echo("No output file name provided, set your output file name using -o / --output, default will be used (first 10 characters of the text)")
+        click.echo(click.style("No output file name provided, set your output file name using -o / --output, default will be used (first 10 characters of the text)", fg="yellow"))
         output = text[:10] if len(text) >= 10 else text
     if len(output) > 35:
-        click.echo("Output file name too long, please use a name with less than 35 characters")
-        return sys.exit(1)
+        raise click.BadParameter(click.style("Output file name too long, please use a name with less than 35 characters", fg="bright_red"))
     if len(text) > 100:
-        click.echo("Text too long, please use a text with less than 100 characters")
-        return sys.exit(1)
+        raise click.BadParameter(click.style("Text too long, please use a text with less than 100 characters", fg="bright_red"))
 
-    if model == "audiogen" and len(text) < 100:
-        generate_text_audio(text, duration, output)
-    elif model == "musicgen" and len(text) < 100:
-        generate_text_music(text, duration, output)
+    if model == "audiogen":
+        audiogen_model = DEFAULT_AUDIOGEN_MODEL
+        generate_text_audio(text, duration, output, audiogen_model)
+    elif model == "musicgen":
+        musicgen_model = DEFAULT_MUSICGEN_MODEL
+        generate_text_music(text, duration, output, musicgen_model)
