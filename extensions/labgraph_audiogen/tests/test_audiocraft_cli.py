4b4671f1f56f5bb5d42b8811f0c326c8bd3643a4
Omar Macias <a01635795@tec.mx>
2023-11-10 10:58:16 -0600

    Testing(MusicGen CLI): Created a test for the generation with the model and its workflow

---
+++ extensions/labgraph_audiogen/tests/test_audiocraft_cli.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+
+def test_text_to_smallmusicgen():
+    """
+    Test the music generation from text with the musicgen-small model
+    """
+    # Run the lg_audiogen -m musicgen-small -d 3 -o test_audio.wav "A happy song"
+    subprocess.run(["lg_audiogen", "-m", "musicgen-small", "-d", "3", "-o", "test_audio", "A happy song"],
+                   capture_output=True, text=True)
+    # Assert file exists and size
+    assert os.path.exists("outputs/test_audio0.wav"), "The file test_audio0.wav does not exist"
+    assert os.path.getsize("outputs/test_audio0.wav") > 0, "The file test_audio0.wav is empty"
+    # Remove the file
+    os.remove("outputs/test_audio0.wav")
+    os.rmdir("outputs")
