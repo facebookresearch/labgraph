4e48d1ce0a34f5452f1bdff593f220f967fde38d
Omar Macias <a01635795@tec.mx>
2023-11-16 15:43:16 -0600

    Tests(MusicGen CLI Testing): Debug of a test that was working previously

--- extensions/labgraph_audiogen/tests/test_audiocraft_cli.py
+++ extensions/labgraph_audiogen/tests/test_audiocraft_cli.py
@@ -11,10 +11,10 @@ def test_text_to_smallmusicgen():
     # Run the lg_audiogen -m musicgen-small -d 3 -o test_audio.wav "A happy song"
     print(subprocess.run(
         ["lg_audiogen", "-m", "musicgen-small", "-d", "3", "-o", "test_audio", "A happy song"],
-        capture_output=True, text=True, check=True))
-    print(subprocess.run(["ls"], capture_output=True, text=True, check=True))
-    print(os.path.exists("test_audio0.wav"))
+        capture_output=True, text=True))
     # Assert file exists and size
+    print(subprocess.run(["ls", "-l"], capture_output=True, text=True))
+    print(subprocess.run(["pwd"], capture_output=True, text=True))
     assert os.path.exists("outputs/test_audio0.wav"), "The file test_audio0.wav does not exist"
     assert os.path.getsize("outputs/test_audio0.wav") > 0, "The file test_audio0.wav is empty"
     # Remove the file
