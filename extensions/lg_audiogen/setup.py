1317661db46fe1fcae00216bd2fcae9c048a5620
Nate8888 <wilk.nathan@gmail.com>
2023-11-16 19:44:08 -0500

    Add versions + Improve descriptions

--- extensions/lg_audiogen/setup.py
+++ extensions/lg_audiogen/setup.py
@@ -3,12 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='lg_audiogen',
     version='0.1',
-    description="Audio generation on labgraph",
+    description="A Command-line interface to use Audiocraft for labgraph",
+    long_description="""
+    A Command-line interface to facilitate the usage of Audiocraft's models
+    to generate and process audio on labgraph
+    """,
     packages=find_packages(),
     install_requires=[
-        'Click',
-        "torchaudio",
-        "audiocraft",
+        "Click>=8.1.7",
+        "torch>=2.1.0",
+        "torchaudio>=2.1.0",
+        "audiocraft==1.1.0",
     ],
     entry_points='''
         [console_scripts]
