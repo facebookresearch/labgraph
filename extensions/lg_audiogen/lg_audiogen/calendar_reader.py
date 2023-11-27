0fc95f9ad95431c33b2cdce038d3b38a3e7bd547
Nate8888 <wilk.nathan@gmail.com>
2023-11-27 00:53:05 -0500

    handles recurring events and set event limits

--- extensions/lg_audiogen/lg_audiogen/calendar_reader.py
+++ extensions/lg_audiogen/lg_audiogen/calendar_reader.py
@@ -1,5 +1,9 @@
 from icalendar import Calendar
 from datetime import datetime, date, timedelta
+from dateutil.rrule import rrulestr
+
+MAX_YEAR = 2023
+CURRENT_YEAR = datetime.now().year
 
 # Read the user's calendar file and parse it into an icalendar object
 with open('calendar.ics', 'r') as f:
@@ -8,6 +12,10 @@ with open('calendar.ics', 'r') as f:
 # holds data in the format {'2023-11-06': [Event]} of the user's calendar
 calendar_events = {}
 
+# We only to get the events from the current year to the max year
+def is_within_limit(dt):
+    return CURRENT_YEAR <= dt.year <= MAX_YEAR
+
 for component in gcal.walk():
     if component.name == "VEVENT":
         # Extract information about the event
@@ -15,11 +23,26 @@ for component in gcal.walk():
         start_dt = component.get('dtstart').dt
         end_dt = component.get('dtend').dt
         duration = int((end_dt - start_dt).total_seconds() / 60)  # duration in minutes
-        
-        dt_str = start_dt.strftime('%Y-%m-%d') # Dict key is the date in the format YYYY-MM-DD
-        if dt_str not in calendar_events:
-          calendar_events[dt_str] = []
-        
-        calendar_events[dt_str].append({'name': summary, 'duration': duration})
 
-print(calendar_events)
+        # Calculate the recurrence rule, if any
+        if 'rrule' in component:
+            rr = rrulestr(component.get('rrule').to_ical().decode('utf-8'), dtstart=start_dt)
+            for dt in rr:
+                if not is_within_limit(dt):
+                    continue
+                dt_str = dt.strftime('%Y-%m-%d')
+                if dt_str not in calendar_events:
+                    calendar_events[dt_str] = []
+                calendar_events[dt_str].append({'name': summary, 'duration': duration})
+        else:
+            dt_str = start_dt.strftime('%Y-%m-%d')
+            if dt_str not in calendar_events:
+                calendar_events[dt_str] = []
+            calendar_events[dt_str].append({'name': summary, 'duration': duration})
+
+for date, events in sorted(calendar_events.items()):
+    print(f"Date: {date}")
+    for event in events:
+        print(f"  Event: {event['name']}")
+        print(f"  Duration: {event['duration']} minutes")
+    print()
