112db66991cdb3f5dbc661dfaf59222240c7bbe3
Nate8888 <wilk.nathan@gmail.com>
2023-11-27 02:18:46 -0500

    Refactor code into functions & remove redundancies

--- extensions/lg_audiogen/lg_audiogen/calendar_reader.py
+++ extensions/lg_audiogen/lg_audiogen/calendar_reader.py
@@ -2,48 +2,57 @@ from icalendar import Calendar
 from datetime import datetime, date, timedelta
 from dateutil.rrule import rrulestr
 
-MAX_YEAR = 2023
-CURRENT_YEAR = datetime.now().year
+# Inclusive [2021, 2021]
+MIN_YEAR = datetime.now().year
+MAX_YEAR = MIN_YEAR 
 
-# Read the user's calendar file and parse it into an icalendar object
-with open('calendar.ics', 'r') as f:
-    gcal = Calendar.from_ical(f.read())
-
-# holds data in the format {'2023-11-06': [Event]} of the user's calendar
-calendar_events = {}
-
-# We only to get the events from the current year to the max year
+# We only to get the events from the minimum year to the max year
 def is_within_limit(dt):
-    return CURRENT_YEAR <= dt.year <= MAX_YEAR
+    return MIN_YEAR <= dt.year <= MAX_YEAR
 
-for component in gcal.walk():
-    if component.name == "VEVENT":
-        # Extract information about the event
-        summary = str(component.get('summary'))
-        start_dt = component.get('dtstart').dt
-        end_dt = component.get('dtend').dt
-        duration = int((end_dt - start_dt).total_seconds() / 60)  # duration in minutes
+def populate_events(component, start_dt, calendar_events, summary, duration):
+    if not is_within_limit(start_dt):
+        return 0 
+    dt_str = start_dt.strftime('%Y-%m-%d')
+    if dt_str not in calendar_events:
+        calendar_events[dt_str] = []
+    calendar_events[dt_str].append({'name': summary, 'duration': duration})
+    return 1
 
-        # Builds up the missing events that are defined by the recurring rules
-        # Ex: Meetings that happen every M, W, F
-        if 'rrule' in component:
-            # rr is a generator
-            rr = rrulestr(component.get('rrule').to_ical().decode('utf-8'), dtstart=start_dt)
-        for dt in rr:
-            if not is_within_limit(dt): # Year Out of bounds
-                break
-            dt_str = dt.strftime('%Y-%m-%d')
-            if dt_str not in calendar_events:
-                calendar_events[dt_str] = []
-            calendar_events[dt_str].append({'name': summary, 'duration': duration})
-        else:
-            dt_str = start_dt.strftime('%Y-%m-%d')
-            if not is_within_limit(start_dt):
-                continue
-            if dt_str not in calendar_events:
-                calendar_events[dt_str] = []
-            calendar_events[dt_str].append({'name': summary, 'duration': duration})
+def populate_recurring_events(component, start_dt, calendar_events, summary, duration):
+    # rr will give us a generator
+    rr = rrulestr(component.get('rrule').to_ical().decode('utf-8'), dtstart=start_dt)
+    for dt in rr:
+        if populate_events(component, dt, calendar_events, summary, duration) == 0:
+            return # short circuit if we're out of the range
 
+
+def calendar_to_dictionary(filepath):
+    # Read the user's calendar file and parse it into an icalendar object
+    with open(filepath, 'r', encoding='utf-8') as f:
+        gcal = Calendar.from_ical(f.read())
+
+    # holds data in the format {'2023-11-06': [Event]} of the user's calendar
+    calendar_events = {}
+
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            # Extract information about the event
+            summary = str(component.get('summary'))
+            start_dt = component.get('dtstart').dt
+            end_dt = component.get('dtend').dt
+            duration = int((end_dt - start_dt).total_seconds() / 60)  # duration in minutes
+
+            # rrule Builds up the missing events that are defined by the recurring rules
+            # Ex: Meetings that happen every M, W, F
+            if 'rrule' in component:
+                populate_recurring_events(component, start_dt, calendar_events, summary, duration)
+            else:
+                populate_events(component, start_dt, calendar_events, summary, duration)
+
+    return calendar_events
+    
+calendar_events = calendar_to_dictionary('calendar.ics')
 for date, events in sorted(calendar_events.items()):
     print(f"Date: {date}")
     for event in events:
